1a2c009fb049f85463e17ace2f415f3bd8fad17e
jerrydev01 <jerrydev01@gmail.com>
2022-10-12 14:39:59 -0700

    m

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,23 +1,49 @@
-#ifndef _CALC_H_
-#define _CALC_H_
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * struct op - Struct op
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
  *
- * @op: The operator
- * @f: The function associated
+ * Return: void
  */
-typedef struct op
+void print_opcodes(char *a, int n)
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	int i;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
 
-#endif /* _CALC_H_ */
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
+}
